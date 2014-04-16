983be628cac6c3fd3852c24d46478641cc08d090
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-04-16 10:37:04 +0200

    Support using material based vat calculation as a VAT Type
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

--- sql/postgresql/intranet-invoices-create.sql
+++ sql/postgresql/intranet-invoices-create.sql
@@ -1335,3 +1335,8 @@ where label = 'invoices_providers_new_bill';
 
 \i ../common/intranet-invoices-backup.sql
 
+SELECT acs_log__debug('/packages/intranet-invoices/sql/postgresql/upgrade/upgrade-4.0.5.0.1-4.0.5.0.2.sql','');
+
+-- Add category that marks the VAT to be calculated on line item level
+SELECT im_category_new (42021,'Material Based Taxation','Intranet VAT Type');
+update im_categories set aux_int1=0 where category_id = 42021;
